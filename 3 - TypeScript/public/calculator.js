4f055b8d0181738ed552db829454429c941e8b03
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-03-01 13:51:35 +0100

    Before demo

---
+++ 3 - TypeScript/public/calculator.js
@@ -0,0 +1,17 @@
+﻿var x = document.getElementById('x');
+var y = document.getElementById('y');
+var result = document.getElementById('result');
+var add = document.getElementById('add');
+
+function Calculator() {
+}
+
+Calculator.prototype.add = function (x, y) {
+    return x + y;
+};
+
+
+add.addEventListener('click', function() {
+    var calc = new Calculator();
+    result.innerText = calc.add(x.value, y.value);
+});
