bbf6737122a3bf270973aa3cab661e00744c6935
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-03-31 09:00:25 +0200

    Start of demo

---
+++ 5 - Framework Architectuur/public/demo.js
@@ -0,0 +1,15 @@
+/**
+ * Created by maurice on 3/31/2016.
+ */
+
+$(function(){
+
+    $("#toOne").click(function(){
+        window.location = 'one.html';
+    });
+
+    $("#toTwo").click(function(){
+        window.location = 'two.html';
+    });
+
+});
