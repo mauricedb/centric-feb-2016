a098bf5b1c955667d89e9fb586a40cd62f6a2477
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-04-06 19:44:46 +0200

    Demo code

--- 5 - Framework Architectuur/public/demo.js
+++ 5 - Framework Architectuur/public/demo.js
@@ -5,11 +5,24 @@
 $(function(){
 
     $("#toOne").click(function(){
-        window.location = 'one.html';
+        //window.location = 'one.html';
+        //$('#content').load('one.html');
+        window.location = "#one";
     });
 
     $("#toTwo").click(function(){
-        window.location = 'two.html';
+        //window.location = 'two.html';
+        //$('#content').load('two.html');
+        window.location = "#two";
     });
 
+    $(window).on('hashchange', function(){
+
+        $('#content').load(window.location.hash.substr(1) + '.html');
+
+    })
+
+    if (window.location.hash) {
+        $('#content').load(window.location.hash.substr(1) + '.html');
+    }
 });
