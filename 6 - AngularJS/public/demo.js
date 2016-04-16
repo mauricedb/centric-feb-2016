acfc73f41d48f8aed635b3eb485744f751aecc14
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-04-16 11:50:19 +0200

    Demo code

--- 6 - AngularJS/public/demo.js
+++ 6 - AngularJS/public/demo.js
@@ -3,17 +3,50 @@
  */
 
 
-(function(){
-    var app = angular.module('app', []);
+(function () {
 
-    app.controller('DemoController', function($scope){
+    var app = angular.module('myApp', ['ngRoute']);
 
-        $scope.person = {
+    app.factory('demoSvc', function($http){
+
+
+        return {
+            hello: function( firstName){
+                console.log('Hello' , firstName);
+            }
+        }
+    });
+
+    app.controller('DemoController', function ($scope, $http, demoSvc) {
+
+        console.log('In demo controller');
+
+        var model = {
             firstName: 'Maurice',
             lastName: 'de Beijer'
         };
 
+        demoSvc.hello(model.firstName);
 
+        $scope.model = model;
+
+
+        $scope.people = [
+
+            model,
+            {
+                firstName: 'Jan',
+                lastName: 'de Beijer'
+            }, {
+                firstName: 'Piet',
+                lastName: 'de Beijer'
+            }, {
+                firstName: 'Klaas',
+                lastName: 'de Beijer'
+            }]
+
+        $scope.addPerson = function(){
+            $scope.people.push({firstName : ''});
+        }
     })
-
 }());
